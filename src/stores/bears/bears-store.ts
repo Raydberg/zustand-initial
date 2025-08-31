11e3821c1287edd762e166d49aa670c365741533
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-31 18:13:41 -0500

    feat: integrate Tailwind CSS and implement bear management functionality

---
+++ src/stores/bears/bears-store.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+
+
+interface Bear {
+    id: number
+    name: string
+}
+
+interface BearState {
+    blackBears: number
+    polarBearn: number
+    pandaBear: number
+    bears: Bear[]
+
+    computed: {
+        totalBears: number
+    },
+
+    increaseBlackBear: (by: number) => void
+    increasePolarBear: (by: number) => void
+    increasePandaBear: (by: number) => void
+    doNothing: () => void
+    addBear: () => void
+    clearBear: () => void
+}
+
+
+export const useBearStore = create<BearState>()((set, get) => ({
+    blackBears: 10,
+    polarBearn: 5,
+    pandaBear: 1,
+    bears: [{ id: 1, name: "Oso 1" }],
+
+    computed: {
+        get totalBears(): number {
+            return get().blackBears + get().polarBearn + get().pandaBear
+        }
+    },
+
+    increaseBlackBear: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
+    increasePolarBear: (by: number) => set((state) => ({ polarBearn: state.polarBearn + by })),
+    increasePandaBear: (by: number) => set((state) => ({ pandaBear: state.pandaBear + by })),
+    doNothing: () => set(state => ({ bears: [...state.bears] })),
+    // doNothing: () => set(state => ({ bears: [...get().bears] })), //Lo mismo que ...state.bears
+    addBear: () => set(state => ({
+        bears: [...state.bears, { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` }]
+    })),
+    clearBear: () => set({ bears: [] }),
+
+}))
